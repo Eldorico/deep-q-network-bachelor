0be1da119cee1914b453690c8c40bec6105e7e11
Eldorico <eldorico@gmail.com>
2018-05-08 22:17:30 +0200

    removed debug message on network

--- src/network.py
+++ src/network.py
@@ -226,8 +226,8 @@ class Network:
         choose_randomly = True if random.random() <= epsilon_value and self.explore else False
 
         # debug
-        if choose_randomly:
-            print("episode %d: choosed randomly. epsilon_value = %f" % (Global.EPISODE_NUMBER, epsilon_value))
+        # if choose_randomly:
+        #     print("episode %d: choosed randomly. epsilon_value = %f" % (Global.EPISODE_NUMBER, epsilon_value))
 
         if choose_randomly:
             action = random.randint(0,self.model.layers[-1].size[1]-1)
