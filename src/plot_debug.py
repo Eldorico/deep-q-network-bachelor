1a9bcfb95cf017480b21047a43b328912197db0e
Eldorico <eldorico@gmail.com>
2018-05-09 17:36:54 +0200

    updated the reward_viewer

--- src/plot_debug.py
+++ src/plot_debug.py
@@ -44,27 +44,28 @@ world.reset()
 
 # fill the Z with the rewards
 X, Y = [i for i in range(world.game_width)], [i for i in range(world.game_height)] # np.mgrid[:world.game_width, :world.game_height]
-Z = np.zeros((world.game_width, world.game_height))
+Z = np.zeros((world.game_height, world.game_width))
 for x in X:
     for y in Y:
         world.agent.x, world.agent.y = x, y
-        Z[world.game_height - 1 - y][x] = reward_function(world)
+        Z[y][x-1] = reward_function(world)
+
 
 # add the ennemies
 for ennemy in world.ennemies:
-    ennemy_y = int(world.game_height - 1 - ennemy.y)
-    ennemy_x = int(ennemy.x)
+    ennemy_y = int(ennemy.y)
+    ennemy_x = int(ennemy.x-1)
     Z[ennemy_y][ennemy_x] = -0.5
 
     # print("%f, %f" %(ennemy.x, ennemy.y))
     # print(Direction.toStr[ennemy.direction])
 
-    start_x_arrow = ennemy_x + Direction.dx[ennemy.direction]
-    start_y_arrow = ennemy_y - Direction.dy[ennemy.direction]
-    end_x_arrow = ennemy_x - Direction.dx[ennemy.direction]
-    end_y_arrow = ennemy_y + Direction.dy[ennemy.direction]
-    plt.annotate('', xy=(start_x_arrow,start_y_arrow), xytext=(end_x_arrow, end_y_arrow),
-                    arrowprops=dict(facecolor='red', shrink=0.05))
+    # start_x_arrow = ennemy_x + Direction.dx[ennemy.direction]
+    # start_y_arrow = ennemy_y - Direction.dy[ennemy.direction]
+    # end_x_arrow = ennemy_x - Direction.dx[ennemy.direction]
+    # end_y_arrow = ennemy_y + Direction.dy[ennemy.direction]
+    # plt.annotate('', xy=(start_x_arrow,start_y_arrow), xytext=(end_x_arrow, end_y_arrow),
+    #                 arrowprops=dict(facecolor='red', shrink=0.05))
 
 pos = plt.imshow(Z, cmap='Greens', interpolation='none')
 plt.colorbar(pos)
