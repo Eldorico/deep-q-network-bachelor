c4195679815a4bd86a05fd580a60f99841ffcfb1
Eldorico <eldorico@gmail.com>
2018-05-19 10:45:33 +0200

    implementing a chronometer to see the time consumed by the training / non training part of the learning prosses agent

---
+++ src/chronometer.py
@@ -0,0 +1,51 @@
+import time
+import matplotlib.pyplot as plt
+
+
+class Singleton(object):
+    _instance = None
+    def __new__(class_, *args, **kwargs):
+        if not isinstance(class_._instance, class_):
+            class_._instance = object.__new__(class_, *args, **kwargs)
+        return class_._instance
+
+
+class Chronometer(Singleton):
+
+    NON_TRAINING_CHRONO = 0
+    TRAINING_CHRONO = 1
+
+    def __init__(self):
+        super().__init__()
+        if hasattr(self, '_current_start_time'):
+            return
+
+        self._current_start_time = { Chronometer.NON_TRAINING_CHRONO: 0, Chronometer.TRAINING_CHRONO: 0}
+        self._current_delta_time = { Chronometer.NON_TRAINING_CHRONO: 0, Chronometer.TRAINING_CHRONO: 0 }
+        self._delta_times_saved = { Chronometer.NON_TRAINING_CHRONO: [0], Chronometer.TRAINING_CHRONO: [0] }
+        self._chrono_paused = { Chronometer.NON_TRAINING_CHRONO: True, Chronometer.TRAINING_CHRONO: True }
+
+    def plot_chrono_deltas(self):
+        plt.plot(self._delta_times_saved[Chronometer.TRAINING_CHRONO],label="Training")
+        plt.plot(self._delta_times_saved[Chronometer.NON_TRAINING_CHRONO], label="non Training")
+        plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0.)
+        plt.show()
+
+    def resume_chrono(self, chrono_to_use):
+        if self._chrono_paused[chrono_to_use]:
+            self._current_start_time[chrono_to_use] = time.time()
+            self._chrono_paused[chrono_to_use] = False
+        else:
+            print("Chrono already resumed")
+
+    def pause_chrono(self, chrono_to_use):
+        if not self._chrono_paused[chrono_to_use]:
+            self._current_delta_time[chrono_to_use] += time.time() - self._current_start_time[chrono_to_use]
+            self._chrono_paused[chrono_to_use] = True
+        else:
+            print("Chrono already paused")
+
+    def checkpoint_chrono(self, chrono_to_use):
+        self._delta_times_saved[chrono_to_use].append(self._current_delta_time[chrono_to_use])
+        self._current_start_time[chrono_to_use] = 0
+        self._current_delta_time[chrono_to_use] = 0
